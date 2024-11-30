6617c22002b5123b8a53d13ea1d4a477394a1118
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2024-11-29 20:54:54 -0500

    Initial configuration

---
+++ StudentScheduler.Application/ApplicationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StudentScheduler.Application
+{
+    public static class ApplicationExtensions
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ StudentScheduler.infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StudentScheduler.infrastructure
+{
+    public static class InfrastructureExtensions
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
